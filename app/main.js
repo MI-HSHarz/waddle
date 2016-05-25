ac43b31ef8bcbe2549bf6d7e873fa398f124bb18
Christian Schrebe <christian@schrebe.de>
2016-05-25 13:16:40 +0200

    disable node integration

--- app/main.js
+++ app/main.js
@@ -20,12 +20,13 @@ function createWindow () {
   });
 
   // Create the browser window.
-  mainWindow = new BrowserWindow(
-      {
-        width: 1024,
-        height: 768
-      }
-  )
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html')
