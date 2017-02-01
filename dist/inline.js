8e0c88c42d64d6041ad59109c556875f01e0695f
SeriousShit <hagen@serious-shit.de>
2017-02-01 13:25:53 +0100

    Bilder verkleinert Umlaute in Kriterienclips entfernt

--- dist/inline.js
+++ dist/inline.js
@@ -1,229 +1,139 @@
-/******/
-(function (modules) { // webpackBootstrap
-    /******/ 	// install a JSONP callback for chunk loading
-    /******/
-    var parentJsonpFunction = window["webpackJsonp"];
-    /******/
-    window["webpackJsonp"] = function webpackJsonpCallback(chunkIds, moreModules, executeModules) {
-        /******/ 		// add "moreModules" to the modules object,
-        /******/ 		// then flag all "chunkIds" as loaded and fire callback
-        /******/
-        var moduleId, chunkId, i = 0, resolves = [], result;
-        /******/
-        for (; i < chunkIds.length; i++) {
-            /******/
-            chunkId = chunkIds[i];
-            /******/
-            if (installedChunks[chunkId])
-            /******/                resolves.push(installedChunks[chunkId][0]);
-            /******/
-            installedChunks[chunkId] = 0;
-            /******/
-        }
-        /******/
-        for (moduleId in moreModules) {
-            /******/
-            if (Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
-                /******/
-                modules[moduleId] = moreModules[moduleId];
-                /******/
-            }
-            /******/
-        }
-        /******/
-        if (parentJsonpFunction) parentJsonpFunction(chunkIds, moreModules, executeModules);
-        /******/
-        while (resolves.length)
-            /******/            resolves.shift()();
-        /******/
-        if (executeModules) {
-            /******/
-            for (i = 0; i < executeModules.length; i++) {
-                /******/
-                result = __webpack_require__(__webpack_require__.s = executeModules[i]);
-                /******/
-            }
-            /******/
-        }
-        /******/
-        return result;
-        /******/
-    };
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	var parentJsonpFunction = window["webpackJsonp"];
+/******/ 	window["webpackJsonp"] = function webpackJsonpCallback(chunkIds, moreModules, executeModules) {
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [], result;
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(installedChunks[chunkId])
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(chunkIds, moreModules, executeModules);
+/******/ 		while(resolves.length)
+/******/ 			resolves.shift()();
+/******/ 		if(executeModules) {
+/******/ 			for(i=0; i < executeModules.length; i++) {
+/******/ 				result = __webpack_require__(__webpack_require__.s = executeModules[i]);
+/******/ 			}
+/******/ 		}
+/******/ 		return result;
+/******/ 	};
 
-    /******/ 	// The module cache
-    /******/
-    var installedModules = {};
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
 
-    /******/ 	// objects to store loaded and loading chunks
-    /******/
-    var installedChunks = {
-        /******/        2: 0
-        /******/
-    };
+/******/ 	// objects to store loaded and loading chunks
+/******/ 	var installedChunks = {
+/******/ 		2: 0
+/******/ 	};
 
-    /******/ 	// The require function
-    /******/
-    function __webpack_require__(moduleId) {
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
 
-        /******/ 		// Check if module is in cache
-        /******/
-        if (installedModules[moduleId])
-        /******/            return installedModules[moduleId].exports;
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
 
-        /******/ 		// Create a new module (and put it into the cache)
-        /******/
-        var module = installedModules[moduleId] = {
-            /******/            i: moduleId,
-            /******/            l: false,
-            /******/            exports: {}
-            /******/
-        };
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
 
-        /******/ 		// Execute the module function
-        /******/
-        modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 
-        /******/ 		// Flag the module as loaded
-        /******/
-        module.l = true;
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
 
-        /******/ 		// Return the exports of the module
-        /******/
-        return module.exports;
-        /******/
-    }
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 
-    /******/ 	// This file contains only the entry chunk.
-    /******/ 	// The chunk loading function for additional chunks
-    /******/
-    __webpack_require__.e = function requireEnsure(chunkId) {
-        /******/
-        if (installedChunks[chunkId] === 0)
-        /******/            return Promise.resolve();
+/******/ 	// This file contains only the entry chunk.
+/******/ 	// The chunk loading function for additional chunks
+/******/ 	__webpack_require__.e = function requireEnsure(chunkId) {
+/******/ 		if(installedChunks[chunkId] === 0)
+/******/ 			return Promise.resolve();
 
-        /******/ 		// an Promise means "currently loading".
-        /******/
-        if (installedChunks[chunkId]) {
-            /******/
-            return installedChunks[chunkId][2];
-            /******/
-        }
-        /******/ 		// start chunk loading
-        /******/
-        var head = document.getElementsByTagName('head')[0];
-        /******/
-        var script = document.createElement('script');
-        /******/
-        script.type = 'text/javascript';
-        /******/
-        script.charset = 'utf-8';
-        /******/
-        script.async = true;
-        /******/
-        script.timeout = 120000;
+/******/ 		// an Promise means "currently loading".
+/******/ 		if(installedChunks[chunkId]) {
+/******/ 			return installedChunks[chunkId][2];
+/******/ 		}
+/******/ 		// start chunk loading
+/******/ 		var head = document.getElementsByTagName('head')[0];
+/******/ 		var script = document.createElement('script');
+/******/ 		script.type = 'text/javascript';
+/******/ 		script.charset = 'utf-8';
+/******/ 		script.async = true;
+/******/ 		script.timeout = 120000;
 
-        /******/
-        script.src = __webpack_require__.p + "" + chunkId + ".chunk.js";
-        /******/
-        var timeout = setTimeout(onScriptComplete, 120000);
-        /******/
-        script.onerror = script.onload = onScriptComplete;
-        /******/
-        function onScriptComplete() {
-            /******/ 			// avoid mem leaks in IE.
-            /******/
-            script.onerror = script.onload = null;
-            /******/
-            clearTimeout(timeout);
-            /******/
-            var chunk = installedChunks[chunkId];
-            /******/
-            if (chunk !== 0) {
-                /******/
-                if (chunk) chunk[1](new Error('Loading chunk ' + chunkId + ' failed.'));
-                /******/
-                installedChunks[chunkId] = undefined;
-                /******/
-            }
-            /******/
-        };
-        /******/
-        head.appendChild(script);
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + ".chunk.js";
+/******/ 		var timeout = setTimeout(onScriptComplete, 120000);
+/******/ 		script.onerror = script.onload = onScriptComplete;
+/******/ 		function onScriptComplete() {
+/******/ 			// avoid mem leaks in IE.
+/******/ 			script.onerror = script.onload = null;
+/******/ 			clearTimeout(timeout);
+/******/ 			var chunk = installedChunks[chunkId];
+/******/ 			if(chunk !== 0) {
+/******/ 				if(chunk) chunk[1](new Error('Loading chunk ' + chunkId + ' failed.'));
+/******/ 				installedChunks[chunkId] = undefined;
+/******/ 			}
+/******/ 		};
+/******/ 		head.appendChild(script);
 
-        /******/
-        var promise = new Promise(function (resolve, reject) {
-            /******/
-            installedChunks[chunkId] = [resolve, reject];
-            /******/
-        });
-        /******/
-        return installedChunks[chunkId][2] = promise;
-        /******/
-    };
+/******/ 		var promise = new Promise(function(resolve, reject) {
+/******/ 			installedChunks[chunkId] = [resolve, reject];
+/******/ 		});
+/******/ 		return installedChunks[chunkId][2] = promise;
+/******/ 	};
 
-    /******/ 	// expose the modules object (__webpack_modules__)
-    /******/
-    __webpack_require__.m = modules;
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
 
-    /******/ 	// expose the module cache
-    /******/
-    __webpack_require__.c = installedModules;
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
 
-    /******/ 	// identity function for calling harmory imports with the correct context
-    /******/
-    __webpack_require__.i = function (value) {
-        return value;
-    };
+/******/ 	// identity function for calling harmory imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
 
-    /******/ 	// define getter function for harmory exports
-    /******/
-    __webpack_require__.d = function (exports, name, getter) {
-        /******/
-        Object.defineProperty(exports, name, {
-            /******/            configurable: false,
-            /******/            enumerable: true,
-            /******/            get: getter
-            /******/
-        });
-        /******/
-    };
+/******/ 	// define getter function for harmory exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		Object.defineProperty(exports, name, {
+/******/ 			configurable: false,
+/******/ 			enumerable: true,
+/******/ 			get: getter
+/******/ 		});
+/******/ 	};
 
-    /******/ 	// getDefaultExport function for compatibility with non-harmony modules
-    /******/
-    __webpack_require__.n = function (module) {
-        /******/
-        var getter = module && module.__esModule ?
-            /******/            function getDefault() {
-                return module['default'];
-            } :
-            /******/            function getModuleExports() {
-                return module;
-            };
-        /******/
-        __webpack_require__.d(getter, 'a', getter);
-        /******/
-        return getter;
-        /******/
-    };
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
 
-    /******/ 	// Object.prototype.hasOwnProperty.call
-    /******/
-    __webpack_require__.o = function (object, property) {
-        return Object.prototype.hasOwnProperty.call(object, property);
-    };
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 
-    /******/ 	// __webpack_public_path__
-    /******/
-    __webpack_require__.p = "";
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
 
-    /******/ 	// on error function for async loading
-    /******/
-    __webpack_require__.oe = function (err) {
-        console.error(err);
-        throw err;
-    };
-    /******/
-})
+/******/ 	// on error function for async loading
+/******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
+/******/ })
 /************************************************************************/
-/******/([]);
+/******/ ([]);
 //# sourceMappingURL=inline.map
